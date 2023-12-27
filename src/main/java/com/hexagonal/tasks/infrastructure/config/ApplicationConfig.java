14f28c4169f3aed6e3d729e7a271550696dfec15
Pablo <musaberpablo@gmail.com>
2023-12-27 17:29:52 -0300

    feat: add infraestructure layer

---
+++ src/main/java/com/hexagonal/tasks/infrastructure/config/ApplicationConfig.java
@@ -0,0 +1,45 @@
+package com.hexagonal.tasks.infrastructure.config;
+
+import com.hexagonal.tasks.application.services.TaskService;
+import com.hexagonal.tasks.application.usecases.*;
+import com.hexagonal.tasks.domain.ports.in.GetAdditionalTaksInfoUseCase;
+import com.hexagonal.tasks.domain.ports.out.ExternalServicePort;
+import com.hexagonal.tasks.domain.ports.out.TaskRepositoryPort;
+import com.hexagonal.tasks.infrastructure.adapters.ExternalServiceAdapter;
+import com.hexagonal.tasks.infrastructure.repositories.JpaTaskRepositoryAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public TaskService taskService(TaskRepositoryPort taskRepositoryPort, GetAdditionalTaksInfoUseCase getAdditionalTaksInfoUseCase){
+        return new TaskService(
+                new CreateTaskUseCaseImpl(taskRepositoryPort),
+                new RetrieveTaskUseCaseImpl(taskRepositoryPort),
+                new UpdateTaskUseCaseImpl(taskRepositoryPort),
+                new DeleteTaskUseCaseImpl(taskRepositoryPort),
+                getAdditionalTaksInfoUseCase
+        );
+    }
+
+    @Bean
+    public TaskRepositoryPort taskRepositoryPort(JpaTaskRepositoryAdapter jpaTaskRepositoryAdapter){
+        return jpaTaskRepositoryAdapter;
+    }
+
+    @Bean
+    public GetAdditionalTaksInfoUseCase getAdditionalTaksInfoUseCase(ExternalServicePort externalServicePort){
+        return new GetAdditionalTaskInfoUseCaseImpl(externalServicePort);
+    }
+
+    @Bean
+    public ExternalServicePort externalServicePort(){
+        return new ExternalServiceAdapter(new RestTemplate());
+    }
+
+}
+
+
---
+++ src/main/java/com/hexagonal/tasks/infrastructure/entities/TaskEntity.java
@@ -0,0 +1,81 @@
+package com.hexagonal.tasks.infrastructure.entities;
+
+import com.hexagonal.tasks.domain.models.Task;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+    private boolean completed;
+
+    public TaskEntity() {
+    }
+
+    public TaskEntity(Long id, String title, String description, LocalDateTime creationDate, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.completed = completed;
+    }
+
+    //Substituir por un mapper
+    public static TaskEntity fromDomainModel(Task task){
+        return new TaskEntity(task.getId(), task.getTitle(), task.getDescription(), task.getCreationDate(), task.isCompleted());
+    }
+
+    public Task toDomainModel(){
+        return new Task(id, title, description, creationDate, completed);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
